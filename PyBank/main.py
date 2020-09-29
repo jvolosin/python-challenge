d271e818ab8a292c5abe49cefb32615f4b844673
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2020-09-29 16:07:49 -0400

    Calculated Financial_Analysis Exported to text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,13 +4,10 @@ import csv
 
 csvpath = os.path.join(r'C:\Users\jvolo\python-challenge\PyBank\Resources\budget_data.csv')
 
-# Set the Variables 
-total_months = 0 
-net_total = 0
-average_change = 0
-greatest_increase = 0
-greatest_decrease = 0
-
+# Create lists for the following variables
+total_months = []
+total_profit = []
+monthly_profit_change = []
 
 with open(csvpath) as csvfile:
 
@@ -18,12 +15,53 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     # Don't count the header
-    header = next(csvreader) 
+    header = next(csvreader)  
 
     for row in csvreader: 
 
-         # Count the number of months
-        total_months +=1
+        # Append the total months and total profit to their corresponding lists
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
 
-print(f"Total Months: {total_months}")
+    # Get the monthly change in profits
+    for i in range(len(total_profit)-1):
+        
+        # Take the difference between two months and append to monthly profit change
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+        
+# Get the max and min of the the monthly profit change
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
+
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+
+# Print the results
+print(f"Financial Analysis")
+print(f"----------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+# Export a text file with the results
+output_file = os.path.join("python-challenge", "PyBank", "Financial_Analysis.txt")
+
+with open(output_file,"w") as file:
+    
+# Print to Financial_Analysis file
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("---------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
 
