6ebe5d3f391523260b7273ad4e20357dd4d39c48
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2020-09-29 08:41:29 -0400

    Calculated winning voter, percentages and votes

---
+++ PyPoll/main.py
@@ -0,0 +1,72 @@
+import os
+
+import csv
+
+csvpath = os.path.join(r'C:\Users\jvolo\python-challenge\PyPoll\Resources\election_data.csv')
+
+# Set the Variables 
+total_votes = 0 
+correy_votes = 0
+khan_votes = 0
+li_votes = 0
+otooley_votes = 0
+
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Don't count the header
+    header = next(csvreader)   
+    
+    
+    for row in csvreader: 
+
+        # Count the number of votes
+        total_votes +=1
+
+        
+
+# Count how many times a name appears and keep in a list
+        if row[2] == "Khan": 
+            khan_votes +=1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li": 
+            li_votes +=1
+        elif row[2] == "O'Tooley":
+            otooley_votes +=1        
+
+# Make a dictionary to find the winner
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+
+
+# Get the winner using a max function of the dictionary 
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+# Print the percentages
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+# Print the results
+print(f"Election Results")
+print(f"-------------------")
+print(f"Total Votes: {total_votes}")
+print(f"------------------_")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"------------------")
+print(f"Winner: {key}")
+print(f"------------------")
+    
+
+
+
+
